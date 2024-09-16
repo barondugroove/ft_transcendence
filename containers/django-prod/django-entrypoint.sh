1b598de052f1e7154b60b1f0fcfa9a7f6183ac51
barondugroove <chabot.benjamin@gmail.com>
2024-09-16 22:03:24 +0200

    prepare setup for server

--- containers/django-prod/django-entrypoint.sh
+++ containers/django-prod/django-entrypoint.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
-if [ "$DB_NAME" = "transcendence" ]
-then
-    echo "Waiting for postgres..."
 
-    while ! nc -z $DB_HOST $DB_PORT; do
-      sleep 0.1
+# Check if we are using the 'transcendence' database
+if [ "$DB_NAME" = "transcendence" ]; then
+    echo "Waiting for PostgreSQL..."
+
+    # Wait for PostgreSQL to be ready (add a timeout of 30 seconds to avoid infinite loops)
+    timeout=30
+    while ! nc -z "$DB_HOST" "$DB_PORT"; do
+        timeout=$((timeout - 1))
+        if [ $timeout -eq 0 ]; then
+            echo "PostgreSQL is not available after waiting. Exiting..."
+            exit 1
+        fi
+        sleep 1
     done
 
     echo "PostgreSQL started"
 fi
 
+# Run Django database migrations
 python manage.py migrate
 
+# Start Daphne server with SSL options
 # daphne -e ssl:port=8000:interface=10.13.249.37:privateKey=./ssl/nginx.key:certKey=./ssl/nginx.crt back.routing:application
+
+# Execute the CMD instruction from the Dockerfile (e.g., Daphne)
 exec "$@"
