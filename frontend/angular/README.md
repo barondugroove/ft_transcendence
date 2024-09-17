94c8cf16fc49e79502313221826b85f0afdf1954
barondugroove <chabot.benjamin@gmail.com>
2024-09-17 17:22:50 +0200

    prepare setup for server

--- frontend/angular/README.md
+++ frontend/angular/README.md
@@ -1,4 +1,4 @@
-# Transcendance
+# Transcendence
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.8.
 
--- frontend/angular/src/app/app.component.spec.ts
+++ frontend/angular/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'transcendance'`, () => {
+  it(`should have as title 'transcendence'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('transcendance');
+    expect(app.title).toEqual('transcendence');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('transcendance app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('transcendence app is running!');
   });
 });
