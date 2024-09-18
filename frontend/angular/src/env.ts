23e7d243d555c88ac723b86a82a8306d1e236914
barondugroove <chabot.benjamin@gmail.com>
2024-09-18 14:08:18 +0200

    prepare setup for server

--- frontend/angular/src/env.ts
+++ frontend/angular/src/env.ts
@@ -1,4 +1,4 @@
 export const PROD_MODE = true
-export const IP_SERVER = PROD_MODE ? '13.51.106.93:8000' : '127.0.0.1:8000'
+export const IP_SERVER = PROD_MODE ? 'django:8000' : '127.0.0.1:8000'
 export const HTTP_MODE = PROD_MODE ? 'https://' : 'http://'
 export const WS_MODE = PROD_MODE ? 'wss://' : 'ws://'
