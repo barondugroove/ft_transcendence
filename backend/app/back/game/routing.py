518189d363a5a473a3961874bf07eb8e1257dc96
barondugroove <chabot.benjamin@gmail.com>
2024-09-19 16:58:00 +0200

    changed backend urls to feature a /api

--- backend/app/back/game/routing.py
+++ backend/app/back/game/routing.py
@@ -3,6 +3,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/game/(?P<game>[0-9a-f-]+)/$", consumers.PlayerConsumer.as_asgi()),
-    re_path(r"ws/game/queue/", consumers.QueueManager.as_asgi()),
+    re_path(r"api/ws/game/(?P<game>[0-9a-f-]+)/$", consumers.PlayerConsumer.as_asgi()),
+    re_path(r"api/ws/game/queue/", consumers.QueueManager.as_asgi()),
 ]
