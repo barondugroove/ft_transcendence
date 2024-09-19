cddb308de6111afc1eae3562d110a06dcbad9cce
barondugroove <chabot.benjamin@gmail.com>
2024-09-19 16:43:02 +0200

    setup almost finished need to correct frontend backend communication

--- frontend/angular/src/env.ts
+++ frontend/angular/src/env.ts
@@ -1,4 +1,4 @@
 export const PROD_MODE = true
-export const IP_SERVER = '13.51.106.93:8000'
+export const IP_SERVER = '13.51.106.93:8000/api'
 export const HTTP_MODE = PROD_MODE ? 'https://' : 'http://'
 export const WS_MODE = PROD_MODE ? 'wss://' : 'ws://'
