bf6a593bd51e25a8ca269fcb9209f4e4d004bd98
barondugroove <chabot.benjamin@gmail.com>
2024-10-07 17:45:56 +0200

    ssl configuration

--- frontend/angular/src/env.ts
+++ frontend/angular/src/env.ts
@@ -1,4 +1,4 @@
 export const PROD_MODE = true
-export const IP_SERVER = '13.51.48.204:8000/api';
+export const IP_SERVER = 'transcendence.benjaminchabot.dev/api';
 export const HTTP_MODE = PROD_MODE ? 'https://' : 'http://'
 export const WS_MODE = PROD_MODE ? 'wss://' : 'ws://'
